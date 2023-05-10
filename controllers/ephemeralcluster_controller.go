22b936ec98a135b5da5b88553690bdcc7d7f4b23
Vibhav Bobade <vibhav.bobde@gmail.com>
2023-05-10 08:41:02 +0530

    create vcluster with helm installed

--- controllers/ephemeralcluster_controller.go
+++ controllers/ephemeralcluster_controller.go
@@ -17,11 +17,17 @@ limitations under the License.
 package controllers
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
+	"fmt"
+	v1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"math/rand"
+	"os/exec"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/source"
@@ -36,6 +42,14 @@ import (
 type EphemeralClusterReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
+}
+
+// Helm values for the vcluster chart
+type HelmValuesInit struct {
+	Manifests string `json:"manifests"`
+}
+type HelmValues struct {
+	Init HelmValuesInit `json:"init"`
 }
 
 const (
@@ -61,21 +75,27 @@ const (
 func (r *EphemeralClusterReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	logger := log.FromContext(ctx)
 
-	loftHelmRepo := &fluxsourcev1.HelmRepository{
-		ObjectMeta: ctrl.ObjectMeta{
-			Name:      LOFT_HELM_REPO,
-			Namespace: req.Namespace,
-		},
-		Spec: fluxsourcev1.HelmRepositorySpec{
-			URL: "https://charts.loft.sh",
+	fluxYAML, err := getFluxInstallOutput()
+	if err != nil {
+		fmt.Printf("Error running flux install command: %v\n", err)
+		return ctrl.Result{}, err
+	}
+
+	vclusterHelmValues := HelmValues{
+		Init: HelmValuesInit{
+			Manifests: fluxYAML,
 		},
 	}
 
-	err := r.Create(ctx, loftHelmRepo)
-	// check if error is because the HelmRepository already exists
-	if err != nil && !strings.Contains(err.Error(), "already exists") {
-		logger.Info("Error while creating HelmRepository", "", err)
+	// marshal HelmValues struct to JSON
+	helmValuesRaw, err := json.Marshal(vclusterHelmValues)
+	if err != nil {
+		fmt.Printf("Error marshaling JSON: %v\n", err)
+		return ctrl.Result{}, err
 	}
+
+	// Create the apiextensionsv1.JSON instance with the raw data
+	helmValuesJSONObj := v1.JSON{Raw: helmValuesRaw}
 
 	// For each EphemeralCluster custom resource, check if there is a HelmRelease
 	// List all the HelmRelease custom resources and check if there are any with
@@ -97,17 +117,36 @@ func (r *EphemeralClusterReconciler) Reconcile(ctx context.Context, req ctrl.Req
 	}
 
 	for _, eCluster := range eClusterList.Items {
-		correspondingHelmReleaseNameSuffix := ECLUSTER_NAME_SUFFIX + eCluster.Name
+		helmReleaseNameSuffix := ECLUSTER_NAME_SUFFIX + eCluster.Name
 
 		// Check if there is a HelmRelease with the corresponding name
 		// If there isn't, then create a new HelmRelease
-		if !helmReleaseExists(correspondingHelmReleaseNameSuffix, helmReleaseList) {
-			helmReleaseName := correspondingHelmReleaseNameSuffix + "-" + randString(5)
+		if !helmReleaseExists(helmReleaseNameSuffix, helmReleaseList) {
+			// Set release name
+			helmReleaseName := helmReleaseNameSuffix + "-" + randString(5)
+
+			// Create HelmRepository in the same namespace as the HelmRelease
+			loftHelmRepo := &fluxsourcev1.HelmRepository{
+				ObjectMeta: ctrl.ObjectMeta{
+					Name:      LOFT_HELM_REPO,
+					Namespace: eCluster.Namespace,
+				},
+				Spec: fluxsourcev1.HelmRepositorySpec{
+					URL: "https://charts.loft.sh",
+				},
+			}
+
+			err = r.Create(ctx, loftHelmRepo)
+			// check if error is because the HelmRepository already exists
+			if err != nil && !strings.Contains(err.Error(), "already exists") {
+				logger.Info("Error while creating HelmRepository", "", err)
+			}
+
 			// Create a new HelmRelease
 			newHelmRelease := &fluxhelmv2beta1.HelmRelease{
 				ObjectMeta: ctrl.ObjectMeta{
 					Name:      helmReleaseName,
-					Namespace: helmReleaseName,
+					Namespace: eCluster.Namespace,
 				},
 				Spec: fluxhelmv2beta1.HelmReleaseSpec{
 					Chart: fluxhelmv2beta1.HelmChartTemplate{
@@ -122,8 +161,16 @@ func (r *EphemeralClusterReconciler) Reconcile(ctx context.Context, req ctrl.Req
 						},
 					},
 					ReleaseName: helmReleaseName,
-					Values:      nil,
+					Values:      &helmValuesJSONObj,
 				},
+			}
+
+			// TODO: implement alternative to SetControllerReference where the owner reference is set
+			// in the annotations of the HelmReleases.
+			//
+			// Set the owner reference for the HelmRelease object
+			if err := controllerutil.SetControllerReference(&eCluster, newHelmRelease, r.Scheme); err != nil {
+				return ctrl.Result{}, err
 			}
 
 			err = r.Create(ctx, newHelmRelease)
@@ -153,6 +200,17 @@ func randString(n int) string {
 		b[i] = letterRunes[rand.Intn(len(letterRunes))]
 	}
 	return string(b)
+}
+
+func getFluxInstallOutput() (string, error) {
+	cmd := exec.Command("flux", "install", "--namespace=flux-system", "--components=source-controller,helm-controller", "--toleration-keys=sandbox.gke.io/runtime", "--export")
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		return "", err
+	}
+	return out.String(), nil
 }
 
 // SetupWithManager sets up the controller with the Manager.
