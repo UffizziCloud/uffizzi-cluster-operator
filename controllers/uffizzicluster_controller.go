67c043cbdaf0ce802b042ffed45cc874a3ffcca1
Vibhav Bobade <vibhav.bobde@gmail.com>
2023-06-16 14:48:51 +0530

    feat: upgrade vcluster to 0.15.2

--- controllers/uffizzicluster_controller.go
+++ controllers/uffizzicluster_controller.go
@@ -50,7 +50,7 @@ const (
 	UCLUSTER_NAME_PREFIX   = "uc-"
 	LOFT_HELM_REPO         = "loft"
 	VCLUSTER_CHART         = "vcluster"
-	VCLUSTER_CHART_VERSION = "0.15.0"
+	VCLUSTER_CHART_VERSION = "0.15.2"
 	LOFT_CHART_REPO_URL    = "https://charts.loft.sh"
 	INGRESS_CLASS_NGINX    = "nginx"
 )
