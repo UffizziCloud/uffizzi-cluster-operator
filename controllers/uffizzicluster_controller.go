5776b8a18f4ec77e64d1f81d68b0a657d9d8ff42
Vibhav Bobade <vibhav.bobde@gmail.com>
2023-08-14 22:29:28 +0530

    plugin: update uffizzi plugin version

--- controllers/uffizzicluster_controller.go
+++ controllers/uffizzicluster_controller.go
@@ -404,7 +404,7 @@ func (r *UffizziClusterReconciler) upsertVClusterHelmRelease(update bool, ctx co
 		},
 		Plugin: VClusterPlugins{
 			VClusterPlugin{
-				Image:           "uffizzi/ucluster-sync-plugin:v0.2.1",
+				Image:           "uffizzi/ucluster-sync-plugin:v0.2.2",
 				ImagePullPolicy: "IfNotPresent",
 				Rbac: VClusterRbac{
 					Role: VClusterRbacRole{
