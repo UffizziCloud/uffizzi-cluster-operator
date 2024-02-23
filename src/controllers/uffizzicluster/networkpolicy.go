9c5a6392492b60a971560af5debbb00b9ac6593a
Vibhav Bobade <vibhav.bobde@gmail.com>
2024-02-23 18:02:07 +0530

    refactor: consolidate source  (#88)
    * refactor: cosolidate source
    
    * fix: paths in dockerfile

---
+++ src/controllers/uffizzicluster/networkpolicy.go
@@ -0,0 +1,66 @@
+package uffizzicluster
+
+import (
+	"fmt"
+	uclusteruffizzicomv1alpha1 "github.com/UffizziCloud/uffizzi-cluster-operator/src/api/v1alpha1"
+	"github.com/UffizziCloud/uffizzi-cluster-operator/src/controllers/helm/build/vcluster"
+	corev1 "k8s.io/api/core/v1"
+	networkingv1 "k8s.io/api/networking/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+)
+
+func (r *UffizziClusterReconciler) buildEgressPolicy(uCluster *uclusteruffizzicomv1alpha1.UffizziCluster) *networkingv1.NetworkPolicy {
+	port443 := intstr.FromInt(443)
+	port80 := intstr.FromInt(80)
+	TCP := corev1.ProtocolTCP
+	uClusterHelmReleaseName := vcluster.BuildVClusterHelmReleaseName(uCluster)
+	egressPolicy := &networkingv1.NetworkPolicy{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      fmt.Sprintf("%s-workloads-ingress", uClusterHelmReleaseName),
+			Namespace: uCluster.Namespace,
+		},
+		Spec: networkingv1.NetworkPolicySpec{
+			Egress: []networkingv1.NetworkPolicyEgressRule{
+				{
+					Ports: []networkingv1.NetworkPolicyPort{
+						{
+							Port:     &port443,
+							Protocol: &TCP,
+						},
+						{
+							Port:     &port80,
+							Protocol: &TCP,
+						},
+					},
+					To: []networkingv1.NetworkPolicyPeer{
+						{
+							PodSelector: &metav1.LabelSelector{
+								MatchLabels: map[string]string{
+									"app.kubernetes.io/component": "controller",
+									"app.kubernetes.io/name":      "ingress-nginx",
+								},
+							},
+						},
+						{
+							NamespaceSelector: &metav1.LabelSelector{
+								MatchLabels: map[string]string{
+									"kubernetes.io/metadata.name": "uffizzi",
+								},
+							},
+						},
+					},
+				},
+			},
+			PodSelector: metav1.LabelSelector{
+				MatchLabels: map[string]string{
+					"vcluster.loft.sh/managed-by": uClusterHelmReleaseName,
+				},
+			},
+			PolicyTypes: []networkingv1.PolicyType{
+				networkingv1.PolicyTypeEgress,
+			},
+		},
+	}
+	return egressPolicy
+}
---
+++ src/controllers/uffizzicluster/statefulset.go
@@ -0,0 +1,71 @@
+package uffizzicluster
+
+import (
+	"context"
+	"errors"
+	"github.com/UffizziCloud/uffizzi-cluster-operator/src/api/v1alpha1"
+	uffizzicluster "github.com/UffizziCloud/uffizzi-cluster-operator/src/controllers/etcd"
+	"github.com/UffizziCloud/uffizzi-cluster-operator/src/controllers/helm/build/vcluster"
+	appsv1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"time"
+)
+
+var (
+	ErrStatefulSetNil = errors.New("statefulSet is nil")
+)
+
+func (r *UffizziClusterReconciler) getUffizziClusterStatefulSet(ctx context.Context, uCluster *v1alpha1.UffizziCluster) (*appsv1.StatefulSet, error) {
+	ucStatefulSet := &appsv1.StatefulSet{}
+	if err := r.Get(ctx, types.NamespacedName{
+		Name:      vcluster.BuildVClusterHelmReleaseName(uCluster),
+		Namespace: uCluster.Namespace}, ucStatefulSet); err != nil {
+		return nil, err
+	}
+	return ucStatefulSet, nil
+}
+
+func (r *UffizziClusterReconciler) getEtcdStatefulSet(ctx context.Context, uCluster *v1alpha1.UffizziCluster) (*appsv1.StatefulSet, error) {
+	etcdStatefulSet := &appsv1.StatefulSet{}
+	if err := r.Get(ctx, types.NamespacedName{
+		Name:      uffizzicluster.BuildEtcdHelmReleaseName(uCluster),
+		Namespace: uCluster.Namespace}, etcdStatefulSet); err != nil {
+		return nil, err
+	}
+	return etcdStatefulSet, nil
+}
+
+// scaleStatefulSets scales the stateful set to the given scale
+func (r *UffizziClusterReconciler) scaleStatefulSets(ctx context.Context, scale int, statefulSets ...*appsv1.StatefulSet) error {
+	// if the current replicas is greater than 0, then scale down to 0
+	replicas := int32(scale)
+	for _, ss := range statefulSets {
+		if ss != nil {
+			ss.Spec.Replicas = &replicas
+			if err := r.Update(ctx, ss); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+// waitForStatefulSetToScale is a goroutine which waits for the stateful set to be ready
+func (r *UffizziClusterReconciler) waitForStatefulSetToScale(ctx context.Context, scale int, ucStatefulSet *appsv1.StatefulSet) error {
+	if ucStatefulSet == nil {
+		return ErrStatefulSetNil
+	}
+	// wait for the StatefulSet to be ready
+	return wait.PollImmediate(time.Second*5, time.Minute*1, func() (bool, error) {
+		if err := r.Get(ctx, types.NamespacedName{
+			Name:      ucStatefulSet.Name,
+			Namespace: ucStatefulSet.Namespace}, ucStatefulSet); err != nil {
+			return false, err
+		}
+		if ucStatefulSet.Status.AvailableReplicas == int32(scale) {
+			return true, nil
+		}
+		return false, nil
+	})
+}
