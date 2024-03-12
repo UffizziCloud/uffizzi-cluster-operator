591608c33bd08e008ad82d505acc61dcff4bdde2
Vibhav Bobade <vibhav.bobde@gmail.com>
2024-03-12 23:57:11 +0530

    fix hack script location

--- hack/minikube-patch-tolerations.sh
+++ hack/minikube-patch-tolerations.sh
@@ -1,11 +1,11 @@
 kubectl get deployments --all-namespaces -o jsonpath="{range .items[*]}{.metadata.namespace}{' '}{.metadata.name}{'\n'}{end}" | while read -r line; do
   namespace=$(echo "$line" | cut -d' ' -f1)
   deployment=$(echo "$line" | cut -d' ' -f2)
-  kubectl patch deployment "$deployment" --patch-file="../.github/kubectl-patch/nodeselector-toleration.yaml" -n "$namespace"
+  kubectl patch deployment "$deployment" --patch-file="./.github/kubectl-patch/nodeselector-toleration.yaml" -n "$namespace"
 done
 
 kubectl get statefulset --all-namespaces -o jsonpath="{range .items[*]}{.metadata.namespace}{' '}{.metadata.name}{'\n'}{end}" | while read -r line; do
   namespace=$(echo "$line" | cut -d' ' -f1)
   statefulset=$(echo "$line" | cut -d' ' -f2)
-  kubectl patch statefulset "$statefulset" --patch-file="../.github/kubectl-patch/nodeselector-toleration.yaml" -n "$namespace"
+  kubectl patch statefulset "$statefulset" --patch-file="./.github/kubectl-patch/nodeselector-toleration.yaml" -n "$namespace"
 done
