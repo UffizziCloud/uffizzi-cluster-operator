d6e043cea0771ab723af55647267b3e144d38847
Vibhav Bobade <vibhav.bobde@gmail.com>
2024-03-14 06:01:57 +0530

    run copied test

--- src/test/e2e/uffizzicluster_controller_test.go
+++ src/test/e2e/uffizzicluster_controller_test.go
@@ -81,27 +81,6 @@ var _ = Describe("k3s: explicit nodeselector and toleration", func() {
 	testUffizziCluster.Run(ctx)
 })
 
-var _ = Describe("k3s: explicit nodeselector and toleration", func() {
-	BeforeEach(func() {
-		if !e2e.IsTainted {
-			Skip("Skipping test because cluster is not tainted")
-		}
-	})
-	ctx := context.Background()
-	tolerations := append([]v1.Toleration{}, vcluster.GvisorToleration.ToV1())
-	testUffizziCluster := TestDefinition{
-		Name: "k3s-nodeselector-toleration-test",
-		Spec: v1alpha1.UffizziClusterSpec{
-			NodeSelector: vcluster.GvisorNodeSelector,
-			Toleration:   tolerations,
-		},
-		ExpectedStatus: initExpectedStatusOverLifetime(),
-	}
-	testUffizziCluster.ExpectedStatus.Ready.NodeSelector = vcluster.GvisorNodeSelector
-	testUffizziCluster.ExpectedStatus.Ready.Tolerations = tolerations
-	testUffizziCluster.Run(ctx)
-})
-
 var _ = Describe("k3s: nodeselector template - gvisor", func() {
 	BeforeEach(func() {
 		if !e2e.IsTainted {
