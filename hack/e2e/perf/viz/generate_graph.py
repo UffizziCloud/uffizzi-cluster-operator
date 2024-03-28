b5bf2a26e068fbccefb82e30043007f32e402fda
Vibhav Bobade <vibhav.bobde@gmail.com>
2024-03-28 18:17:59 +0530

    update script

--- hack/e2e/perf/viz/generate_graph.py
+++ hack/e2e/perf/viz/generate_graph.py
@@ -1,19 +1,21 @@
-import json
 import matplotlib.pyplot as plt
+import json
 
-# Load the data from data.json
+# Load the data from 'data.json'
 with open('data.json', 'r') as file:
     data = json.load(file)
 
-# Extract workers and time data
+# Extract 'workers' and 'time' into separate lists
 workers = [item['workers'] for item in data]
 time = [item['time'] for item in data]
 
-# Create a graph
+# Create a plot
 plt.figure(figsize=(10, 6))
-plt.plot(workers, time, marker='o')
-plt.title('Time taken to create UffizziClusters')
+plt.plot(workers, time, marker='o', linestyle='-')
+plt.title('Time Taken by UffizziClusters with Varying Workers')
 plt.xlabel('Number of Workers')
-plt.ylabel('Time (s)')
+plt.ylabel('Time')
 plt.grid(True)
+
+# Save the plot as an image file
 plt.savefig('graph.png')
